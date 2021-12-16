6afe2c5fae67ca0d4a5a81fa67029b4849af9ef7
Ammar Afzal <ammarafzal075@gmail.com>
2021-12-16 16:21:55 +0500

    Fix Model

--- lib/Models/event_fitness.dart
+++ lib/Models/event_fitness.dart
@@ -1,10 +1,13 @@
+// To parse this JSON data, do
+//
+//     final eventFitness = eventFitnessFromJson(jsonString);
+
+import 'package:meta/meta.dart';
 import 'dart:convert';
 
-List<EventFitness> eventFitnessFromJson(String str) => List<EventFitness>.from(
-    json.decode(str).map((x) => EventFitness.fromJson(x)));
+List<EventFitness> eventFitnessFromJson(String str) => List<EventFitness>.from(json.decode(str).map((x) => EventFitness.fromJson(x)));
 
-String eventFitnessToJson(List<EventFitness> data) =>
-    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String eventFitnessToJson(List<EventFitness> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class EventFitness {
   EventFitness({
@@ -26,24 +29,24 @@ class EventFitness {
   Topic topic;
 
   factory EventFitness.fromJson(Map<String, dynamic> json) => EventFitness(
-        id: json["id"],
-        topicId: json["topic_id"],
-        name: json["name"],
-        createdAt: DateTime.parse(json["created_at"]),
-        updatedAt: DateTime.parse(json["updated_at"]),
-        days: List<Day>.from(json["days"].map((x) => Day.fromJson(x))),
-        topic: Topic.fromJson(json["topic"]),
-      );
+    id: json["id"],
+    topicId: json["topic_id"],
+    name: json["name"],
+    createdAt: DateTime.parse(json["created_at"]),
+    updatedAt: DateTime.parse(json["updated_at"]),
+    days: List<Day>.from(json["days"].map((x) => Day.fromJson(x))),
+    topic: Topic.fromJson(json["topic"]),
+  );
 
   Map<String, dynamic> toJson() => {
-        "id": id,
-        "topic_id": topicId,
-        "name": name,
-        "created_at": createdAt.toIso8601String(),
-        "updated_at": updatedAt.toIso8601String(),
-        "days": List<dynamic>.from(days.map((x) => x.toJson())),
-        "topic": topic.toJson(),
-      };
+    "id": id,
+    "topic_id": topicId,
+    "name": name,
+    "created_at": createdAt.toIso8601String(),
+    "updated_at": updatedAt.toIso8601String(),
+    "days": List<dynamic>.from(days.map((x) => x.toJson())),
+    "topic": topic.toJson(),
+  };
 }
 
 class Day {
@@ -72,34 +75,30 @@ class Day {
   String name;
 
   factory Day.fromJson(Map<String, dynamic> json) => Day(
-        id: json["id"],
-        topicId: json["topic_id"],
-        monthId: json["month_id"],
-        day: json["day"] == null ? null : json["day"],
-        detail: json["detail"],
-        createdAt: DateTime.parse(json["created_at"]),
-        updatedAt: DateTime.parse(json["updated_at"]),
-        challenges: json["challenges"] == null
-            ? null
-            : List<Day>.from(json["challenges"].map((x) => Day.fromJson(x))),
-        dayId: json["day_id"] == null ? null : json["day_id"],
-        name: json["name"] == null ? null : json["name"],
-      );
+    id: json["id"],
+    topicId: json["topic_id"],
+    monthId: json["month_id"],
+    day: json["day"] == null ? null : json["day"],
+    detail: json["detail"],
+    createdAt: DateTime.parse(json["created_at"]),
+    updatedAt: DateTime.parse(json["updated_at"]),
+    challenges: List<Day>.from(json["challenges"].map((x) => Day.fromJson(x))),
+    dayId: json["day_id"] == null ? null : json["day_id"],
+    name: json["name"] == null ? null : json["name"],
+  );
 
   Map<String, dynamic> toJson() => {
-        "id": id,
-        "topic_id": topicId,
-        "month_id": monthId,
-        "day": day == null ? null : day,
-        "detail": detail,
-        "created_at": createdAt.toIso8601String(),
-        "updated_at": updatedAt.toIso8601String(),
-        "challenges": challenges == null
-            ? null
-            : List<dynamic>.from(challenges.map((x) => x.toJson())),
-        "day_id": dayId == null ? null : dayId,
-        "name": name == null ? null : name,
-      };
+    "id": id,
+    "topic_id": topicId,
+    "month_id": monthId,
+    "day": day == null ? null : day,
+    "detail": detail,
+    "created_at": createdAt.toIso8601String(),
+    "updated_at": updatedAt.toIso8601String(),
+    "challenges": challenges == null ? null : List<dynamic>.from(challenges.map((x) => x.toJson())),
+    "day_id": dayId == null ? null : dayId,
+    "name": name == null ? null : name,
+  };
 }
 
 class Topic {
@@ -116,16 +115,16 @@ class Topic {
   DateTime updatedAt;
 
   factory Topic.fromJson(Map<String, dynamic> json) => Topic(
-        id: json["id"],
-        name: json["name"],
-        createdAt: DateTime.parse(json["created_at"]),
-        updatedAt: DateTime.parse(json["updated_at"]),
-      );
+    id: json["id"],
+    name: json["name"],
+    createdAt: DateTime.parse(json["created_at"]),
+    updatedAt: DateTime.parse(json["updated_at"]),
+  );
 
   Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-        "created_at": createdAt.toIso8601String(),
-        "updated_at": updatedAt.toIso8601String(),
-      };
+    "id": id,
+    "name": name,
+    "created_at": createdAt.toIso8601String(),
+    "updated_at": updatedAt.toIso8601String(),
+  };
 }
