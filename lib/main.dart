26773bdd23eb8f67681d67d9a0ecbc1039adb3c1
Uzair Baloch <uzairbaloch@Uzairs-MacBook-Pro.local>
2021-12-30 20:47:47 +0500

    Music Player

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,21 @@
-import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
-import 'Components/meditation_details.dart';
 import 'Routes/route.dart';
 import 'dart:async';
 
-late AudioHandler audioHandler;
+//late AudioHandler audioHandler;
 var mediaUrl = "";
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  audioHandler = await AudioService.init(
-    builder: () => AudioPlayerHandler(),
-    config: const AudioServiceConfig(
-      androidNotificationChannelId: 'com.ryanheise.myapp.channel.audio',
-      androidNotificationChannelName: 'Audio playback',
-      androidNotificationOngoing: true,
-    ),
-  );
+  // audioHandler = await AudioService.init(
+  //   builder: () => AudioPlayerHandler(),
+  //   config: const AudioServiceConfig(
+  //     androidNotificationChannelId: 'com.ryanheise.myapp.channel.audio',
+  //     androidNotificationChannelName: 'Audio playback',
+  //     androidNotificationOngoing: true,
+  //   ),
+  // );
 
   runApp(
     GetMaterialApp(
